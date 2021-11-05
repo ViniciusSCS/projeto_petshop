4476d4ac414a2dacbbde315ea988fd0a85053356
Vinicius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2021-11-05 09:54:39 -0300

    Ajuste nos códigos

--- petshop_back/app/Http/Controllers/UsuarioController.php
+++ petshop_back/app/Http/Controllers/UsuarioController.php
@@ -144,38 +144,4 @@ class UsuarioController extends Controller
         $user->token = $user->createToken($user->email)->accessToken;
         return ['status' => true, "usuario" => $user];
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
